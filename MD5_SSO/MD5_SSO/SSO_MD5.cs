fd7abac3d304fe8a90f93d40ed5f98fac82b8c31
Raúl Sandoval Meléndez <rsandoval@ecrubio.com>
2017-12-17 17:05:38 -0700

    .

--- MD5_SSO/MD5_SSO/SSO_MD5.cs
+++ MD5_SSO/MD5_SSO/SSO_MD5.cs
@@ -14,14 +14,14 @@ namespace SSO_MD5
             var tiempo = (DateTime.UtcNow.Subtract(new DateTime(1970,1,1))).TotalSeconds.ToString();
             return String.Format("{0}/login/sso?name={1}&email={2}&timestamp={3}&hash={4}",URLBase,Usuario,Correo,tiempo,Hash(Secret,Usuario,Correo,tiempo));
         }
-        private string Hash(string Secret, string Usuario, string Correo, string tiempo)
+        private string Hash(string Secret,string Usuario,string Correo,string tiempo)
         {
             var cadena = Usuario + Secret + Correo + tiempo;
             var keybytes = Encoding.UTF8.GetBytes(Secret);
             var inputBytes = Encoding.UTF8.GetBytes(cadena);
             var crypto = new HMACMD5(keybytes);
             var hash = crypto.ComputeHash(inputBytes);
-            return hash.Select(b => b.ToString("x2")).Aggregate(new StringBuilder(), (current, next) => current.Append(next), current => current.ToString());
+            return hash.Select(b => b.ToString("x2")).Aggregate(new StringBuilder(),(current,next) => current.Append(next),current => current.ToString());
         }
     }
 }
